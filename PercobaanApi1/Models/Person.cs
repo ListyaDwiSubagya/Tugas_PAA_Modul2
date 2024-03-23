7b066f1413cbcbf745017c37ce57faff79cb8c40
ListyaDwiSubagya <listyodwi062@gmail.com>
2024-03-23 16:23:34 +0700

    first

---
+++ PercobaanApi1/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace PercobaanApi1.Models
+{
+    public class Person
+    {
+        public int id_person { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string email { get; set; }
+
+    }
+    public class Murid
+    {
+        public int id_person { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string email { get; set; }
+
+    }
+}
